342bb4e27fc5ceeaec96949e2de1bb75730f986b
Karol Czarnecki <karcio@Dell.fritz.box>
2019-10-31 00:03:58 +0000

    display error

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.get(
 
 function getWeight(req, res, next) {
   pool.connect((err, client, done) => {
-    //console.log('err=', err);
+    console.log('err=', err);
     const query = "SELECT * FROM weight_log order by date desc";
     client.query(query, (error, result) => {
       done();
