8eeeae66f81a0efe6e1da360e2619c98c9960ebc
karcio <karcio@linux.fritz.box>
2020-06-21 22:13:38 +0100

    travis integration

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,5 @@
-su - postgres
-psql -p 5432
-
+--su - postgres
+psql
 \c weightdb
-ALTER USER dbuser WITH SUPERUSER;
-INSERT INTO 'weight_log' (date, weight) VALUES (current_date, 85.5);
+INSERT INTO weight_log (date, weight) VALUES (current_date, 85.5);
 SELECT * FROM weight_log;
