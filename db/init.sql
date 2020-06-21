1860de5c0dd4b0569e3ac332569a636ee0732441
karcio <karcio@linux.fritz.box>
2020-06-21 22:44:13 +0100

    tr

--- db/init.sql
+++ db/init.sql
@@ -1,13 +1,10 @@
 --su - postgres
 --psql -p 5432
 \c weightdb
-
 CREATE TABLE IF NOT EXISTS weight_log(
   id SERIAL PRIMARY KEY,
   date DATE NOT NULL,
   weight NUMERIC(4,1) NOT NULL
 );
-\dt
-
 INSERT INTO weight_log (date, weight) VALUES (current_date, 85.5);
 SELECT * FROM weight_log;
